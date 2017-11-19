41eb6a11e9ef91a52c5242208e22da840246ad2c
Mio <imuy@foxmail.com>
2017-11-19 23:29:23 +0800

    add daily test

---
+++ Ajax/Php/02/file.php
@@ -0,0 +1,19 @@
+<?php
+	header("content-type:text/html;charset=utf-8");
+	// 让php延迟3s执行
+	//sleep(3);
+	//print_r($_FILES);
+	// Array ( [upFile] => Array ( [name] => black.jpg [type] => image/jpeg [tmp_name] => D:\wamp\tmp\php3FA6.tmp [error] => 0 [size] => 3286 ) )
+
+	// 获取原文件名
+	$fileArr = $_FILES['upFile'];
+	$fileName = $fileArr['name'];
+	echo $fileName.'<br>';
+
+	// 获取保存在服务器的位置
+	$filePath = $fileArr['tmp_name'];
+	echo $filePath.'<br>';
+
+	//保存提交的原文件
+	move_uploaded_file($filePath,'./'.$fileName);
+?>
---
+++ Ajax/post_senddata.php
@@ -0,0 +1,5 @@
+<?php
+	// $str = file_get_contents('php://input');
+	// echo $str;
+	echo $_POST['userName'];
+?>
