b6d85a3516210be6111adacf810d2e465328849c
Miot <1405726849@qq.com>
2018-01-29 23:58:56 +0800

    add daily demo

---
+++ NodeJS/__dirname&__filename.js
@@ -0,0 +1,4 @@
+// 当前文件所在的文件路径
+console.log(__dirname);
+// 当前文件全路径（包括本文件名）
+console.log(__filename);
---
+++ NodeJS/add.js
@@ -0,0 +1,6 @@
+var add = function( v1, v2 ){
+	return v1 + v2;
+}
+
+// 每个都一个module对象// console.log(module);
+module.exports = add;
