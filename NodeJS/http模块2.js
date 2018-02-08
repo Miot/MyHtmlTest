3c2b3d81a707f146b4ec1861ee54e4b4b3c1c622
Mio <1405726849@qq.com>
2018-02-09 05:06:29 +0800

    add daily demo

--- NodeJS/http模块2.js
+++ NodeJS/http模块2.js
@@ -1,9 +1,10 @@
 'use strict'
 const http = require('http');
 const fs = require('fs');
+const querystring = require('querystring');
 const server = http.createServer();
 
-server.on('request',function( req, res ){
+server.on('request', function( req, res ){
 	if(req.url == '/1.html' && req.method == 'GET'){
 		fs.readFile('./1.html', 'utf8', function(err,data){
 			res.end(data);
@@ -12,6 +13,18 @@ server.on('request',function( req, res ){
 		fs.readFile('./2.js', 'utf8', function(err,data){
 			res.end(data);
 		})
+	}else if(req.url == '/getData' && req.method == 'POST'){
+		var data = '';
+		req.on('data', function(chunk){
+			data = chunk + data;
+		});
+		req.on('end', function(){
+			console.log('原'+data);
+			var dataObj = querystring.parse(data);
+			console.log(dataObj);
+			res.end('123');
+		});
+		
 	}
 });
 server.listen(4000);
